ece82a3a660f93e920b15c62009eb9f46e97e87d
Joshua Adewole <adewole.josh@gmail.com>
2021-03-18 14:30:42 +0100

    Converted into a Class
    - Fixed standard route: now prints "NO such cities" for unidentifiable route
    - Prints the distance for idnetifiable routes

--- route.py
+++ route.py
@@ -1,23 +1,60 @@
-#  graph input
-graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
 
-# function that outputs for route
-def find(route):
-	route_list = route.split('-')
-	distance = []
+class Route:
 
-	# check for each route splits
-	# A-B-C-D-E
-	for i in range(route_list-1):
-		city = route_list[i]+route_list[i+1]
 
-		for j in range(graph):
-			dist = graph[j].split()
+	def graph_input(self):
+		graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
+		return graph
 
-			for k in range(1):
-				cities = dist[k] + dist[k+1]
 
-				if city == cities:
-					distance.append(dist[2])
+	def graph_dictionary(self, graph):
+		dictionary = {}
 
-	return sum(distance)
+		for j in range(len(graph)):
+			cities_distances = graph[j].split()
+			cities = list(cities_distances[0])
+
+			dictionary[f'{cities[0]}{cities[1]}'] = f'{cities[2]}'
+
+		return dictionary
+
+	def city_list(self, graph):
+		lists = []
+
+		for j in range(len(graph)):
+			cities_distances = graph[j].split()
+			cities = list(cities_distances[0])
+
+			lists.append(f'{cities[0]}{cities[1]}')
+
+		return lists
+
+	# function that outputs for route
+	def findstandardpath(self, route=input("Enter a path to find: ")):
+		graph = self.graph_input()
+		route_list = route.split('-')
+		distance = []
+		dist_list = self.graph_dictionary(graph)
+		city_list = self.city_list(graph)
+
+		# check for each route splits
+		# A-B-C-D-E
+		for i in range(len(route_list)-1):
+			city = str(route_list[i] + route_list[i+1])
+
+			if city in city_list:
+				for k, v in dist_list.items():
+					if k == city:
+						distance.append(int(v))
+
+			else:
+				return print("NO SUCH CITIES")
+
+		length = sum(distance)
+		return print(length)
+
+
+if __name__ == '__main__':
+	
+	t = Route()
+	t.findstandardpath()
