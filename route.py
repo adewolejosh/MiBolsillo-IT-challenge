b655f5c2861451e171f281cc15a09cfd49bc579c
Joshua Adewole <adewole.josh@gmail.com>
2021-03-19 18:33:00 +0100

    - Tidy up finding standard path - Defined functions for finding other required paths - Added a test block that would be used to run tests

--- route.py
+++ route.py
@@ -2,12 +2,13 @@
 class Route:
 
 
-	def graph_input(self):
+	def _graph_input(self):
 		graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
 		return graph
 
 
-	def graph_dictionary(self, graph):
+	# dictionary of all feasible pair nodes and weights
+	def _graph_dictionary(self, graph):
 		dictionary = {}
 
 		for j in range(len(graph)):
@@ -18,7 +19,14 @@ class Route:
 
 		return dictionary
 
-	def city_list(self, graph):
+
+	# list of all nodes available
+	def _city(self, graph):
+		pass
+
+
+	# list of all feasible pair nodes
+	def _city_list(self, graph):
 		lists = []
 
 		for j in range(len(graph)):
@@ -29,13 +37,15 @@ class Route:
 
 		return lists
 
-	# function that outputs for route
-	def findstandardpath(self, route=input("Enter a path to find: ")):
-		graph = self.graph_input()
+
+	# function that outputs for standard route route
+	def findstandardpath(self):
+		route = input("Enter a path to find: ")
+		graph = self._graph_input()
 		route_list = route.split('-')
 		distance = []
-		dist_list = self.graph_dictionary(graph)
-		city_list = self.city_list(graph)
+		dist_list = self._graph_dictionary(graph)
+		city_list = self._city_list(graph)
 
 		# check for each route splits
 		# A-B-C-D-E
@@ -48,13 +58,21 @@ class Route:
 						distance.append(int(v))
 
 			else:
-				return print("NO SUCH CITIES")
+				return print("NO SUCH ROUTES")
 
 		length = sum(distance)
 		return print(length)
 
 
-if __name__ == '__main__':
-	
-	t = Route()
-	t.findstandardpath()
+	# function that outputs trips through a certain path with maximum number of stops
+	def findtripstoandfroapath(self):
+		route = input("Enter two paths: ")
+		maximum_stop = int(input("Enter the maximum no. of stop: "))
+
+
+	def findshortestpath(self):
+		route = input("Enter a route to find the shortest path: ")
+
+
+	def finddifferentpaths(self):
+		route = input("Enter the to and fro path: ")
---
+++ test.py
@@ -0,0 +1,10 @@
+from route import Route
+
+
+def maintest():
+	t = Route()
+	t.findstandardpath()
+
+
+if __name__ == '__main__':
+	maintest()
