fe6a67067d1297dbde0a53a10a21c3cb09745ad9
Joshua Adewole <adewole.josh@gmail.com>
2021-03-20 17:49:33 +0100

    feat: started the function to compute shortest path

--- route.py
+++ route.py
@@ -1,5 +1,8 @@
-''' N.B 
-    some words e.g path, node and city might be used interchangeably
+'''
+
+N.B 
+some words e.g path, node and city might be used interchangeably
+
 '''
 
 import itertools
@@ -7,6 +10,9 @@ import itertools
 
 class Route:
 
+    def __init(self):
+        # self.graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
+        pass
 
     def _graph_input(self):
         graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
@@ -33,8 +39,12 @@ class Route:
         for j in range(len(graph)):
             cities_distances = graph[j].split()
             cities = list(cities_distances[0])
-
-            lists.append(f'{cities[0]}'); lists.append(f'{cities[1]}')
+            a = cities[0]
+            b = cities[1]
+            
+            if a not in lists: lists.append(a)
+            elif b not in lists: lists.append(b)
+            else: continue
 
         return lists
 
@@ -50,6 +60,27 @@ class Route:
             lists.append(f'{cities[0]}{cities[1]}')
 
         return lists
+
+
+    def _standardpath(self, route, graph):
+        distance, dist_list, city_list = [], self._graph_dictionary(graph), self._city_list(graph)
+        route_list = route.split('-')
+        for i in range(len(route_list)-1):
+            city = str(route_list[i] + route_list[i+1])
+
+            if city in city_list:
+                for k, v in dist_list.items():
+                    if k == city:
+                        distance.append(int(v))
+
+            else:
+                distance.append(0)
+
+        if 0 in distance:
+            return False
+        else:
+            return True
+
 
 
     # function that outputs for standard route route
@@ -79,41 +110,19 @@ class Route:
 
 
     # function that outputs trips through a certain path with maximum number of stops
-    def findtripstoandfroapath(self):
-        route = input("Enter two paths: ")
-        graph = self._graph_input()
+    def findtripstoandfroapath(self, route=None, graph=None):
+        route = route or input("Enter two paths: ")
+        graph = graph or self._graph_input()
         maximum_stop = int(input("Enter the maximum no. of stop: "))
         exact_number = int(input("Enter the exact no. of stop: "))
 
         start_route = route.split()[0]
         end_route = route.split()[1]
-
         success = []
 
         # generate possible routes
         list_of_cities = self._city(graph)
         join_together = ''.join(list_of_cities)
-
-
-        def standardpath(route):
-            distance, dist_list, city_list = [], self._graph_dictionary(graph), self._city_list(graph)
-            route_list = route.split('-')
-            for i in range(len(route_list)-1):
-                city = str(route_list[i] + route_list[i+1])
-
-                if city in city_list:
-                    for k, v in dist_list.items():
-                        if k == city:
-                            distance.append(int(v))
-
-                else:
-                    distance.append(0)
-
-            if 0 in distance:
-                return False
-            else:
-                return True
-
 
         if maximum_stop != 0 and exact_number == 0:
             for i in range(maximum_stop):
@@ -122,7 +131,7 @@ class Route:
                 for j in range(len(all_possible_routes)):
                     full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
 
-                    if standardpath(route=full_route) == True:
+                    if self._standardpath(route=full_route, graph=graph) == True:
                         if full_route in success:
                             continue
                         else:
@@ -135,7 +144,7 @@ class Route:
             for j in range(len(all_possible_routes)):
                 full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
 
-                if standardpath(route=full_route) == True:
+                if self._standardpath(route=full_route, graph=graph) == True:
                     if full_route in success:
                         continue
                     else:
@@ -147,8 +156,40 @@ class Route:
         return print(len(success))
 
 
-    def findshortestpath(self):
-        route = input("Enter a route to find the shortest path: ")
+    def findshortestpath(self, route=None, graph=None):
+        route = route or input("Enter a route to find the shortest path: ")
+        graph = graph or self._graph_input()
+        result, distances = [], []
+
+
+        start_route, end_route = route.split()[0], route.split()[1]
+        list_of_cities = self._city(graph)
+        join_together = ''.join(list_of_cities)
+
+        # **************************************************************
+        all_possible_routes = ['-'.join(x) for x in itertools.product(join_together, repeat=len())]
+
+        ######################################################
+        for j in range(len(all_possible_routes)):
+            full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
+
+            if self._standardpath(route=full_route, graph=graph) == True:
+                if full_route in result:
+                    continue
+                else:
+                    result.append(full_route)
+            else:
+                continue
+        #############################################################
+
+        for i in range(len(result)):
+            distances.append(self.findstandardpath(route=result[i]))
+
+        minimum = min(distances)
+
+        for j in distances:
+            if distances[j] == minimum:
+                return result[j]
 
 
     def finddifferentpaths(self):
--- test.py
+++ test.py
@@ -2,9 +2,10 @@ from route import Route
 
 
 def maintest():
-	t = Route()
-	t.findtripstoandfroapath()
+    t = Route()
+    # t.findtripstoandfroapath("A C", "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7")
+    t.findshortestpath("A C")
 
 
 if __name__ == '__main__':
-	maintest()
+    maintest()
