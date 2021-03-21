75dfcdc0306a388203fbf4935889fe019a1044d9
Joshua Adewole <adewole.josh@gmail.com>
2021-03-21 18:12:23 +0100

    Feat: Finalized function to - find shortest path and - different paths that produce less distance

--- route.py
+++ route.py
@@ -1,23 +1,22 @@
-'''
+"""
 
 N.B 
 some words e.g path, node and city might be used interchangeably
 
-'''
+"""
 
 import itertools
 
 
 class Route:
 
-    def __init(self):
+    def __init__(self):
         # self.graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
         pass
 
     def _graph_input(self):
         graph = [entry for entry in input("Enter a list of graph entries : ").split(',')]
         return graph
-
 
     # dictionary of all feasible pair nodes and weights
     def _graph_dictionary(self, graph):
@@ -30,7 +29,6 @@ class Route:
             dictionary[f'{cities[0]}{cities[1]}'] = f'{cities[2]}'
 
         return dictionary
-
 
     # list of all nodes available from graph entries
     def _city(self, graph):
@@ -48,7 +46,6 @@ class Route:
 
         return lists
 
-
     # list of all feasible pair nodes
     def _city_list(self, graph):
         lists = []
@@ -61,7 +58,7 @@ class Route:
 
         return lists
 
-
+    # computes true, if a path is feasible. else, False
     def _standardpath(self, route, graph):
         distance, dist_list, city_list = [], self._graph_dictionary(graph), self._city_list(graph)
         route_list = route.split('-')
@@ -81,11 +78,28 @@ class Route:
         else:
             return True
 
+    def _find_length(self, route, graph):
+        route_list = route.split('-')
+        distance = []
+        dist_list = self._graph_dictionary(graph)
+        city_list = self._city_list(graph)
 
+        # check for each route splits
+        # A-B-C-D-E
+        for i in range(len(route_list)-1):
+            city = str(route_list[i] + route_list[i+1])
+
+            if city in city_list:
+                for k, v in dist_list.items():
+                    if k == city:
+                        distance.append(int(v))
+
+        length = sum(distance)
+        return length
 
     # function that outputs for standard route route
-    def findstandardpath(self):
-        route = input("Enter a path to find: ")
+    def findstandardpath(self, route=None):
+        route = route or input("Enter a path to find: ")
         graph = self._graph_input()
         route_list = route.split('-')
         distance = []
@@ -108,11 +122,10 @@ class Route:
         length = sum(distance)
         return print(length)
 
-
     # function that outputs trips through a certain path with maximum number of stops
-    def findtripstoandfroapath(self, route=None, graph=None):
+    def findtripstoandfroapath(self, route=None):
         route = route or input("Enter two paths: ")
-        graph = graph or self._graph_input()
+        graph = self._graph_input()
         maximum_stop = int(input("Enter the maximum no. of stop: "))
         exact_number = int(input("Enter the exact no. of stop: "))
 
@@ -131,20 +144,20 @@ class Route:
                 for j in range(len(all_possible_routes)):
                     full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
 
-                    if self._standardpath(route=full_route, graph=graph) == True:
+                    if self._standardpath(route=full_route, graph=graph):
                         if full_route in success:
                             continue
                         else:
                             success.append(full_route)
                     else:
                         continue
-        elif maximum_stop == 0  and exact_number != 0:
+        elif maximum_stop == 0 and exact_number != 0:
             all_possible_routes = ['-'.join(x) for x in itertools.product(join_together, repeat=exact_number)]
 
             for j in range(len(all_possible_routes)):
                 full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
 
-                if self._standardpath(route=full_route, graph=graph) == True:
+                if self._standardpath(route=full_route, graph=graph):
                     if full_route in success:
                         continue
                     else:
@@ -152,45 +165,66 @@ class Route:
                 else:
                     continue
 
-
         return print(len(success))
 
-
-    def findshortestpath(self, route=None, graph=None):
+    def findshortestpath(self, route=None):
         route = route or input("Enter a route to find the shortest path: ")
-        graph = graph or self._graph_input()
+        graph = self._graph_input()
         result, distances = [], []
-
 
         start_route, end_route = route.split()[0], route.split()[1]
         list_of_cities = self._city(graph)
+        pair_city = self._city_list(graph)
         join_together = ''.join(list_of_cities)
 
-        # **************************************************************
-        all_possible_routes = ['-'.join(x) for x in itertools.product(join_together, repeat=len())]
+        for i in range(len(list_of_cities)):
+            all_possible_routes = ['-'.join(x) for x in itertools.product(join_together, repeat=i)]
 
-        ######################################################
-        for j in range(len(all_possible_routes)):
-            full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
+            for j in range(len(all_possible_routes)):
+                full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
 
-            if self._standardpath(route=full_route, graph=graph) == True:
-                if full_route in result:
-                    continue
-                else:
-                    result.append(full_route)
-            else:
-                continue
-        #############################################################
+                if self._standardpath(route=full_route, graph=graph):
+                    if full_route in result:
+                        continue
+                    else:
+                        result.append(full_route)
 
-        for i in range(len(result)):
-            distances.append(self.findstandardpath(route=result[i]))
+        for k in range(len(result)):
+            distances.append(int(self._find_length(route=result[k], graph=graph)))
 
         minimum = min(distances)
+        return print(minimum)
 
-        for j in distances:
-            if distances[j] == minimum:
-                return result[j]
+    def finddifferentpaths(self, route=None, maximum_length=None):
+        route = route or input("Enter a route to find the shortest path: ")
+        graph = self._graph_input()
+        maximum_length = maximum_length or input("Enter distance to be less than: ")
+        result, distances = [], []
+
+        start_route, end_route = route.split()[0], route.split()[1]
+        list_of_cities = self._city(graph)
+        pair_city = self._city_list(graph)
+        join_together = ''.join(list_of_cities)
+
+        for i in range(len(pair_city)):
+            all_possible_routes = ['-'.join(x) for x in itertools.product(join_together, repeat=i)]
+
+            for j in range(len(all_possible_routes)):
+                full_route = f'{start_route}-{all_possible_routes[j]}-{end_route}'
+
+                if self._standardpath(route=full_route, graph=graph):
+                    if full_route in result:
+                        continue
+                    else:
+                        result.append(full_route)
+
+        for k in range(len(result)):
+            distances.append(int(self._find_length(route=result[k], graph=graph)))
 
 
-    def finddifferentpaths(self):
-        route = input("Enter the to and fro path: ")
+        success = []
+        for y in range(len(distances)):
+            if distances[y] < int(maximum_length):
+                success.append(result[y])
+
+        return print(len(success))
--- test.py
+++ test.py
@@ -3,8 +3,16 @@ from route import Route
 
 def maintest():
     t = Route()
-    # t.findtripstoandfroapath("A C", "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7")
+    t.findstandardpath("A-B-C")
+    t.findstandardpath("A-D")
+    t.findstandardpath("A-D-C")
+    t.findstandardpath("A-E-B-C-D")
+    t.findstandardpath("A-E-D")
+    t.findtripstoandfroapath("C C", 3, 0)
+    t.findtripstoandfroapath("A C", 0, 4)
     t.findshortestpath("A C")
+    t.findshortestpath("B B")
+    t.finddifferentpaths("C C", 30)
 
 
 if __name__ == '__main__':
